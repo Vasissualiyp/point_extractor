d17b6b311319064d8a17429e430ae5073f5630c5
Vasilii Pusovoit <vasily.pustovoit@mail.utoronto.ca>
2023-08-31 11:58:06 -0400

    Choosing the axes now works slightly better

--- points_extractor.py
+++ points_extractor.py
@@ -1,59 +1,136 @@
-import cv2
 import matplotlib.pyplot as plt
+import time
+
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')  # or any other GUI backend
+
 
 class ExtractPoints:
+
     def __init__(self, image_path):
-        self.image = cv2.imread(image_path)
-        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+
+        self.image = plt.imread(image_path)
+
         self.fig, self.ax = plt.subplots()
+
         self.ax.imshow(self.image)
-        self.coordinates = []
-        self.axis_points = []
-        self.transformation_matrix = None
-        self.set_axes_mode = True
 
-    def collect_points(self, event):
-        ix, iy = event.xdata, event.ydata
-        if self.set_axes_mode:
-            self.axis_points.append((ix, iy))
-            if len(self.axis_points) == 4:  # Collected all axis points
-                self.calculate_transformation_matrix()
-                self.set_axes_mode = False
-                print("Axes set. You can now click on points to get their coordinates.")
-        else:
-            transformed_point = self.transform_point(np.array([ix, iy, 1]))
-            print(f"Point in plot coordinates: {transformed_point}")
+        self.axis_positions = {"x": None, "y": None}
 
-    def calculate_transformation_matrix(self):
-        # Assuming axis_points contains [x_min, x_max, y_min, y_max]
-        axis_coordinates = input("Enter the coordinates of the clicked axis points as x_min, x_max, y_min, y_max separated by commas: ")
-        axis_coordinates = list(map(float, axis_coordinates.split(",")))
+        self.axis_values = {"x": None, "y": None}
 
-        # Solve Ax = B to find the transformation matrix x
-        A = np.array([
-            [self.axis_points[0][0], self.axis_points[0][1], 1, 0, 0, 0],
-            [0, 0, 0, self.axis_points[0][0], self.axis_points[0][1], 1],
-            [self.axis_points[1][0], self.axis_points[1][1], 1, 0, 0, 0],
-            [0, 0, 0, self.axis_points[1][0], self.axis_points[1][1], 1],
-            [self.axis_points[2][0], self.axis_points[2][1], 1, 0, 0, 0],
-            [0, 0, 0, self.axis_points[2][0], self.axis_points[2][1], 1]
-        ])
-        B = np.array([axis_coordinates[0], axis_coordinates[2], axis_coordinates[1], axis_coordinates[2], axis_coordinates[0], axis_coordinates[3]])
+        self.dragging = None
 
-        x = np.linalg.solve(A, B)
-        self.transformation_matrix = np.array([[x[0], x[1], x[2]], [x[3], x[4], x[5]], [0, 0, 1]])
+        self.t0 = None
 
-    def transform_point(self, point):
-        transformed_point = np.dot(self.transformation_matrix, point)
-        return transformed_point[:-1] / transformed_point[-1]  # Homogeneous to Cartesian coordinates
+
+
+    def on_press(self, event):
+
+        self.t0 = time.time()
+
+        if self.axis_positions["x"] is None:
+
+            self.temp_horizontal_line, = self.ax.plot([0, self.image.shape[1]], [event.ydata, event.ydata], 'g--')
+
+        elif self.axis_positions["y"] is None:
+
+            self.temp_vertical_line, = self.ax.plot([event.xdata, event.xdata], [0, self.image.shape[0]], 'g--')
+
+        plt.draw()
+
+
+
+    def on_release(self, event):
+
+        t1 = time.time()
+
+        if t1 - self.t0 > 0.5:  # Long press: > 0.5 seconds
+
+            if self.axis_positions["x"] is None:
+
+                self.axis_positions["x"] = event.ydata
+
+                self.horizontal_line, = self.ax.plot([0, self.image.shape[1]], [event.ydata, event.ydata], 'r-')
+
+                self.temp_horizontal_line.remove()
+
+            elif self.axis_positions["y"] is None:
+
+                self.axis_positions["y"] = event.xdata
+
+                self.vertical_line, = self.ax.plot([event.xdata, event.xdata], [0, self.image.shape[0]], 'r-')
+
+                self.temp_vertical_line.remove()
+
+            plt.draw()
+
+
+
+            if all(v is not None for v in self.axis_positions.values()):
+
+                self.set_axis_values()
+
+
+
+    def set_axis_values(self):
+
+        self.fig.canvas.mpl_disconnect(self.press_event)
+
+        self.fig.canvas.mpl_disconnect(self.release_event)
+
+        x_value = float(input("Enter the x-coordinate value for the x-axis: "))
+
+        y_value = float(input("Enter the y-coordinate value for the y-axis: "))
+
+        self.axis_values["x"] = x_value
+
+        self.axis_values["y"] = y_value
+
+
+
+        self.scale_x = self.axis_positions["y"] / x_value
+
+        self.scale_y = self.axis_positions["x"] / y_value
+
+
+
+        print("You can now click on points to get their coordinates.")
+
+        self.click_event = self.fig.canvas.mpl_connect('button_press_event', self.get_coordinates)
+
+
+
+    def get_coordinates(self, event):
+
+        x, y = event.xdata, event.ydata
+
+        plot_x = x / self.scale_x
+
+        plot_y = y / self.scale_y
+
+        print(f"Point in plot coordinates: x = {plot_x}, y = {plot_y}")
+
+
 
     def start(self):
-        self.fig.canvas.mpl_connect('button_press_event', self.collect_points)
-        print("Please click the 4 points that define the axes: x_min, x_max, y_min, y_max.")
+
+        self.press_event = self.fig.canvas.mpl_connect('button_press_event', self.on_press)
+
+        self.release_event = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
+
+        print("Long press to place the x-axis, then release. Do the same for the y-axis.")
+
         plt.show()
 
+
+
 # Example usage
-# Replace 'your_image.png' with the path to the image you want to use
-# extractor = ExtractPoints('your_image.png')
-# extractor.start()
+
+# Uncomment the last two lines and replace 'your_image.png' with the path to your image.
+
+extractor = ExtractPoints('/home/vasilii/research/sims/MUSIC/plots/31:2/199_PartType1/2Dplot076.png')  
+
+extractor.start()
+
